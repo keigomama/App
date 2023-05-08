a6688dad5d9efd9cf6fb41972af3af547b1a5904
tsuji0241@gmail.com <tsuji0241@gmail.com>
2023-05-09 04:56:04 +0900

    nemutasugiru

---
+++ template/template/templateApp.swift
@@ -0,0 +1,17 @@
+//
+//  templateApp.swift
+//  template
+//
+//  Created by biostr1 on 2023/05/09.
+//
+
+import SwiftUI
+
+@main
+struct templateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
